2bd526e13708207e7ba195651240cd9c28be4483
Dominik Hirt <hirt@todo42.de>
2021-03-18 11:02:56 +0100

    task: extend BookRestController

--- src/main/java/de/workshops/bookdemo/BookRestController.java
+++ src/main/java/de/workshops/bookdemo/BookRestController.java
@@ -8,7 +8,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -34,4 +36,24 @@ public class BookRestController {
     public List<Book> getAllBooks() {
         return books;
     }
+    
+    @GetMapping("/{isbn}")
+    public Book getBookByIsbn(@PathVariable String isbn) {
+        return this.books.stream().filter(book -> hasIsbn(book, isbn)).findFirst().orElseThrow();
+    }
+    
+    @GetMapping(params = "author")
+    public Book getBookByAuthor(@RequestParam String author) {
+        return this.books.stream().filter(book -> hasAuthor(book, author)).findFirst().orElseThrow();
+    }
+    
+    private boolean hasIsbn(Book book, String isbn) {
+        return book.getIsbn().equals(isbn);
+    }
+    
+    private boolean hasAuthor(Book book, String author) {
+        return book.getAuthor().indexOf(author) != -1;
+    }    
+    
+    
 }
